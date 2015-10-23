3a104721b0c458611e5287c4345d6959e29b5cde
Tyrone Trevorrow <tyrone@sudeium.com>
2015-10-23 23:50:02 +1100

    Initial Commit

---
+++ JSONCore/JSONCore.h
@@ -0,0 +1,19 @@
+//
+//  JSONCore.h
+//  JSONCore
+//
+//  Created by Tyrone Trevorrow on 23/10/2015.
+//  Copyright © 2015 Tyrone Trevorrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONCore.
+FOUNDATION_EXPORT double JSONCoreVersionNumber;
+
+//! Project version string for JSONCore.
+FOUNDATION_EXPORT const unsigned char JSONCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONCore/PublicHeader.h>
+
+
