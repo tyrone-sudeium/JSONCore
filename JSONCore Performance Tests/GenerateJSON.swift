906ad458755691702dd5e5ba9a5ac0423a638c02
Tyrone Trevorrow <tyrone@sudeium.com>
2016-03-25 10:42:50 +1100

    Generator for performance test data is now Swift

---
+++ JSONCore Performance Tests/GenerateJSON.swift
@@ -0,0 +1,46 @@
+#!/usr/bin/env xcrun swift
+//
+//  GenerateJSON.swift
+//  JSONCore
+//
+//  Created by Tyrone Trevorrow on 23/03/2016.
+//  Copyright © 2016 Tyrone Trevorrow. All rights reserved.
+//
+
+// It's easier to depend on Foundation for this than to try to self-host!
+import Foundation
+
+let numElements = 1000000
+let arc4random_max = 0x100000000
+var arr = [NSDictionary]()
+
+func randomNumber() -> Double {
+    return Double(arc4random()) / Double(arc4random_max)
+}
+
+func randomName() -> String {
+    var str = ""
+    let chars = Array("abcdefghijklmnopqrstuvwxyz".characters)
+    for _ in 0...5 {
+        let char = chars[Int(arc4random_uniform(UInt32(chars.count)))]
+        str.append(char)
+    }
+    str.appendContentsOf(" ")
+    str.appendContentsOf(arc4random_uniform(10000).description)
+    return str
+}
+
+for _ in 0..<numElements {
+    arr.append([
+        "x": randomNumber(),
+        "y": randomNumber(),
+        "z": randomNumber(),
+        "name": randomName(),
+        "opts": [
+            "1": [1, true]
+        ]
+    ] as NSDictionary)
+}
+
+let data = try! NSJSONSerialization.dataWithJSONObject(arr as NSArray, options: [.PrettyPrinted])
+data.writeToFile("1.json", atomically: true)
